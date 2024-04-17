67e9999877dc388a99f1976a549b4171b7a8cf23
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2024-04-17 13:39:36 -0300

    agregadas entidades Agus

---
+++ Productos/src/main/java/com/TPGrupal2/Productos/Entities/EstadoEnum.java
@@ -0,0 +1,5 @@
+package com.TPGrupal2.Productos.Entities;
+
+public enum EstadoEnum {
+    PREPARACION, PENDIENTE, CANCELADO, RECHAZADO, ENTREGADO
+}
---
+++ Productos/src/main/java/com/TPGrupal2/Productos/Entities/Factura.java
@@ -0,0 +1,27 @@
+package com.TPGrupal2.Productos.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate fechaFacturacion;
+    private int mpMerchantOrderId;
+    private String mpPreferenceId;
+    private String mpPaymentType;
+    private FormaPagoEnum formaPago;
+    private double totalVenta;
+}
